14385eb0b90cfa6abd255430301e525c1f85cc32
MB-matea <mateabeslic1@gmail.com>
2021-09-19 22:24:38 +0200

    added tasks activity

---
+++ app/src/main/java/com/mateabeslic/careapp/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.mateabeslic.careapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<Integer> mRoomNumbers;
+    private ArrayList<String> mNames;
+    private ArrayList<Boolean> mCheckBoxes = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<Integer> mRoomNumbers, ArrayList<String> mNames, ArrayList<Boolean> mCheckBoxes) {
+        this.mRoomNumbers = mRoomNumbers;
+        this.mNames = mNames;
+        this.mContext = mContext;
+        this.mCheckBoxes = mCheckBoxes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        holder.txtRoom.setText(mRoomNumbers.get(position).toString());
+        holder.txtName.setText(mNames.get(position));
+        holder.chkBox.setChecked(mCheckBoxes.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on" + mNames.get(holder.getAdapterPosition()));
+                Toast.makeText(mContext, mNames.get(holder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView txtRoom, txtName;
+        CheckBox chkBox;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtRoom = itemView.findViewById(R.id.txt_room);
+            txtName = itemView.findViewById(R.id.txt_name);
+            chkBox = (CheckBox) itemView.findViewById(R.id.chk_box);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
+
+
--- app/src/main/java/com/mateabeslic/careapp/TasksActivity.java
+++ app/src/main/java/com/mateabeslic/careapp/TasksActivity.java
@@ -1,15 +1,91 @@
 package com.mateabeslic.careapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class TasksActivity extends AppCompatActivity {
+
+    private static final String TAG = "TasksActivity";
+    //var
+    private ArrayList<Integer> mRoomNumbers = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<Boolean> mCheckBoxes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+        
+        initData();
     }
+
+    private void initData() {
+        Log.d(TAG, "initData: initData");
+        mRoomNumbers.add(1);
+        mNames.add("Ivo Ivić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(2);
+        mNames.add("Marin Marić");
+        mCheckBoxes.add(false);
+
+        mRoomNumbers.add(3);
+        mNames.add("Ana Anić");
+        mCheckBoxes.add(false);
+
+        mRoomNumbers.add(4);
+        mNames.add("Toni Tonić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(5);
+        mNames.add("Karla Karlić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(6);
+        mNames.add("Anita Anić");
+        mCheckBoxes.add(false);
+        mRoomNumbers.add(1);
+        mNames.add("Ivo Ivić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(2);
+        mNames.add("Marin Marić");
+        mCheckBoxes.add(false);
+
+        mRoomNumbers.add(3);
+        mNames.add("Ana Anić");
+        mCheckBoxes.add(false);
+
+        mRoomNumbers.add(4);
+        mNames.add("Toni Tonić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(5);
+        mNames.add("Karla Karlić");
+        mCheckBoxes.add(true);
+
+        mRoomNumbers.add(6);
+        mNames.add("Anita Anić");
+        mCheckBoxes.add(false);
+
+        Log.d(TAG, "initData: test");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: initrecyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mRoomNumbers, mNames, mCheckBoxes);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 
 }
