4c60942388d3988829b4a746415a48e6ddf51386
MB-matea <mateabeslic1@gmail.com>
2021-10-12 20:34:04 +0200

    fixed login form

--- app/src/main/java/com/mateabeslic/careapp/BasePath.java
+++ app/src/main/java/com/mateabeslic/careapp/BasePath.java
@@ -5,7 +5,7 @@ public class BasePath {
     // SET BASE PATH
 
     public static String getBasePath(){
-        String basePath = "http://10.201.3.220:8080";
+        String basePath = "http://192.168.0.21:8080";
         return basePath;
     }
 }
